589c95beac6c092db0a64a4b0aba15e255893a50
Aditya Patel <123244634+iampateladitya@users.noreply.github.com>
2023-08-25 17:47:50 +0530

    Add files via upload

---
+++ tut4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+    using namespace std; 
+int main(){
+    int a = 50, b = 60;
+    // int a = 9;
+    // int b = 10;   
+    float ft = 67.6;
+    float flt = 81.6;
+    char c = 'A';  //ek hi character lagana hai bas single quotes hi lagana hai
+    cout<<"Here the value of a is :- "<< a <<". and value of b is :- " << b; 
+    cout<<"\nPercentage of aditya in class 10th is :- " << flt << "%";
+    cout<<"\nPercentage of Aditya in class 12th is :- " << ft << "%";   
+    cout<<"\n Thanks For Visit Our Website Have A Nice Day";
+    cout<<"\nThe First Letter Of Spell Of Aditya Is :- " << c ;
+    return 0;
+}
